99568079eccec4c47ac17e2a434da218bc0b05ca
Torben Arnt Hansen <tarnthansen@gnresound.com>
2020-01-29 14:51:12 +0100

    s

--- src/java/eu/sig/training/ch05/buildandsendmail/BuildAndSendMail.java
+++ src/java/eu/sig/training/ch05/buildandsendmail/BuildAndSendMail.java
@@ -1,13 +1,24 @@
 package eu.sig.training.ch05.buildandsendmail;
 
+public class Person{
+public String firstName;
+public String lastName;
+public String division;
+public Person(String firstName, String lastName, String division ){
+    this.lastName = lastName;
+    this.firstName = firstName;
+    this.division = division;
+}
+
+}
+
 public class BuildAndSendMail {
     // tag::buildAndSendMail[]
-    public void buildAndSendMail(MailMan m, String firstName, String lastName,
-        String division, String subject, MailFont font, String message1,
+    public void buildAndSendMail(MailMan m, Person person, String subject, MailFont font, String message1,
         String message2, String message3) {
         // Format the email address
-        String mId = firstName.charAt(0) + "." + lastName.substring(0, 7) + "@"
-            + division.substring(0, 5) + ".compa.ny";
+        String mId = person.firstName.charAt(0) + "." + person.lastName.substring(0, 7) + "@"
+            + person.division.substring(0, 5) + ".compa.ny";
         // Format the message given the content type and raw message
         MailMessage mMessage = formatMessage(font,
             message1 + message2 + message3);
